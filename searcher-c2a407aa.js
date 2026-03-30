a49247bf280d2e875ba865b111347fb262f12210
ilarioazzollini <ilarioazzollini@users.noreply.github.com>
2026-03-30 15:30:52 +0000

    deploy: c5999a60969ba8abb694d08b0b263a5766b7b831

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9f0630d1.js',
+                path_to_root + 'searchindex-46488493.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
